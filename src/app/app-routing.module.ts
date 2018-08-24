418739b06cb84dbaa213cb07fc973beb6fa0c2b0
Sigfredo <sigfredo13@gmail.com>
2018-08-24 08:50:32 -0300

    Sidebar básica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
+import { DetailsComponent } from './details/details.component';
+import { ProfessoresComponent } from './professores/professores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: '',
+  component: AlunosComponent
+  },
+  {
+  path: 'details/:id',
+  component: DetailsComponent
+  },
+  {
+  path: 'professores',
+  component: ProfessoresComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
